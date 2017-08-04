3c452a03fbf8f3493daa028e0a2ccacdfb44f318
Claudia Zeledon <cjzeledon@gmail.com>
2017-08-04 12:34:25 -0400

    Slowly working on it.

---
+++ main.js
@@ -0,0 +1,20 @@
+// let clear = document.querySelector("input").clear;
+// // let result = document.querySelector
+// // Need to work on the result box
+// let nine = document.querySelector("input").nine;
+// let eight = document.querySelector("input").eight;
+// let seven = document.querySelector("input").seven;
+// let six = document.querySelector("input").six;
+// let five = document.querySelector("input").five;
+// let four = document.querySelector("input").four;
+// let three = document.querySelector("input").three;
+// let two = document.querySelector("input").two;
+// let one = document.querySelector("input").one;
+// let zero = document.querySelector("input").zero;
+// let dot = document.querySelector("input").dotmenot;
+// let equal = document.querySelector("input").equal;
+// let plus = document.querySelector("input").plus;
+
+function calculate(value){
+  document.querySelector("calculator").value=
+}
