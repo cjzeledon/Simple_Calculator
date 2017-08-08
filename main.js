b7557f7a67c161d84034729f9a7c80998bde9168
Claudia Zeledon <cjzeledon@gmail.com>
2017-08-08 12:51:49 -0400

    Working on trying to sum the numbers after they've been parsedInt

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
-// let operation = "0";
-// let maxlength = 50;
-
-function digit(val){
-  document.querySelector("#display").innerHTML += val;
-}
+// array1 = [12];
+// array2 = [10];
+//
+// sum = array1[0] + array2[0];
+//
+// console.log(sum); This gives the sum of 22.
+// Perhaps I can use the idea of pushing the first set of numbers into an array and the other set of numbers into another array and combine the two of them together with an operator (whether it is multiply, minus, add or divide).
 
 function clearMe(nuclear){
   let beClear = document.getElementById("clearOut").value;
   document.querySelector("#display").innerHTML = nuclear;
 }
 
-function total(totalize){
-  if (totalize === "*"){
-    return
+function digit(val){
+  let screen = document.querySelector("#display").innerHTML += val;
+  let num1 = [];
+  let num2 = [];
+
+  for (i = 0; i < screen.length; i++){
+    operator = ["+", "-", "*", "/"];
+
+      if (screen ){
+
+      }
   }
 }
+
+numberTest= parseInt("1293+4567");
+console.log(numberTest);
+
 /*Things to do list:
 1. Create a display area for numbers
 2. Create buttons for numbers 0 to 9
@@ -30,18 +43,18 @@ function total(totalize){
 
 
 
-// /* Note: you can't do the math until you have both numbers */
-// function total(signs){
-//   if (signs === "divide"){
-//     return numValue / numValue;
-//   }
-//   else if (signs === "*"){
-//     return numValue * numValue;
-//   }
-//   else if (signs === "subtract"){
-//     return numValue - numValue;
-//   }
-//   else if (signs === "addition"){
-//     return numValue + numValue;
-//   }
-// }
+/* Note: you can't do the math until you have both numbers */
+function total(signs){
+  if (signs === "/"){
+    return num1 / num2;
+  }
+  else if (signs === "*"){
+    return num1 * num2;
+  }
+  else if (signs === "-"){
+    return num1 - num2;
+  }
+  else if (signs === "+"){
+    return num1 + num2;
+  }
+}
