232a2ab1949e5940f18a7c4419929dc228b8085b
Claudia Zeledon <cjzeledon@gmail.com>
2017-08-10 21:47:40 -0400

    Cleaning up the mess but still working on the calculation as whole.

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-// array1 = [12];
-// array2 = [10];
-//
-// sum = array1[0] + array2[0];
-//
-// console.log(sum); This gives the sum of 22.
-// Perhaps I can use the idea of pushing the first set of numbers into an array and the other set of numbers into another array and combine the two of them together with an operator (whether it is multiply, minus, add or divide).
-
-// let numbers = [];
-// let ops = [];
-
 function clearMe(nuclear){
   let beClear = document.getElementById("clearOut").value;
   document.querySelector("#display").innerHTML = nuclear;
@@ -20,25 +9,20 @@ function digit(value){
 }
 
 function calculate(){
-  let answer = DigtoNums("23+5");
-
-  // SolveEquation(answer);
-  //
-  // ShowResult();
+  let answer = DigtoNums("12+4*5-90");
+  let solveIt = SolveEquation(answer);
+  // let giveResults = ShowResult(solveIt);
 }
 
-// function SolveEquation(thisThing) {
-//
-// }
+let numbers = [];
+let ops = [];
 
 function DigtoNums(convert){
-  let numbers = [];
-  let ops = [];
+  // let numbers = [];
+  // let ops = [];
 
   let NumsDrop = convert.split(/\d/);
-  let OpsDrop = convert.split(/[-=*/]/);
-
-  // console.log(NumsDrop)
+  let OpsDrop = convert.split(/[-=+*/]/);
 
   for (let i = 0; i < NumsDrop.length; i++){
     if (NumsDrop[i] !== ""){
@@ -46,98 +30,47 @@ function DigtoNums(convert){
     }
   }
 
-  for (let i = 0; i < OpsDrop.length; i++){
-    if (OpsDrop[i] !== ""){
-      numbers.push(OpsDrop[i]);
+  for (let j = 0; j < OpsDrop.length; j++){
+    if (OpsDrop[j] !== ""){
+      numbers.push(parseFloat(OpsDrop[j]));
     }
   }
-  console.log(numbers);
-  console.log(ops);
-  return DigitToNums;
+
+  // console.log(numbers);
+  // console.log(ops);
 }
 
-// function calculate(total){
-//
-// }
-  // while (ops.length > 0){
-  //   let operator = ops.shift();
-  //   let
-  // }
 
+function SolveEquation(answer) {
+  // while we still have operators to process...
+  while (ops.length > 0) {
+    // taking the first number from the ops array.
+    // op will be "+"
+    let op = ops.shift();
 
+    // taking the first number from "numbers"
+    // num1 will be "11";
+    let num1 = numbers.shift();
 
+    // taking the first number from numbers AGAIN
+    // num2 will be "23"
+    let num2 = numbers.shift();
 
+    if (op === '+') {
+        // splice puts num1 + num2 into the array
+        // at element 0, removing 0 elements in the
+        // process
+        numbers.splice(0, 0, num1 + num2);
+    } else if (op === '-') {
+        numbers.splice(0, 0, num1 - num2);
+    } else if (op === '*') {
+        numbers.splice(0, 0, num1 * num2);
+    } else if (op === '/') {
+        numbers.splice(0, 0, num1 / num2);
+    }
+  }
+}
 
-  // // sample numbers
-  // let numbers = [11, 23, 45];
-  //
-  // // sample operators between the numbers
-  // let ops = ['+', '-', '*'];
-  //
-  // // while we still have operators to process...
-  // while (ops.length > 0) {
-  //   // taking the first number from the ops array.
-  //   // op will be "+"
-  //   let op = ops.shift();
-  //
-  //   // taking the first number from "numbers"
-  //   // num1 will be "11";
-  //   let num1 = numbers.shift();
-  //
-  //   // taking the first number from numbers AGAIN
-  //   // num2 will be "23"
-  //   let num2 = numbers.shift();
-  //
-  //   if (op === '+') {
-  //       // splice puts num1 + num2 into the array
-  //       // at element 0, removing 0 elements in the
-  //       // process
-  //       numbers.splice(0, 0, num1 + num2);
-  //   } else if (op === '-') {
-  //       numbers.splice(0, 0, num1 - num2);
-  //   }
-  // }
-  //
-  // console.log(numbers[0]);
-  // to get all the numbers in an array:
-  // "1+23-45/3".split(/[-=*/]/);
-
-  // to get all the OPERATORS in an array:
-  // "1+23-45/3".split(/\d/);
-  // remove all empty string elements in the array
-
-
-
-
-
-
-/*Things to do list:
-1. Create a display area for numbers
-2. Create buttons for numbers 0 to 9
-3. Create a dot button, a decimal
-4. Create a button that clears digits
-5. Create a "calculate" button which is the = sign
-6. Determine how to create a max length of the digits added to the display area
-
-- Variables to Create
-  - current --> numbers being displayed
-  - operation
-  - maxlength
-
-
-
-// /* Note: you can't do the math until you have both numbers */
-// function total(signs){
-//   if (signs === "/"){
-//     return num1 / num2;
-//   }
-//   else if (signs === "*"){
-//     return num1 * num2;
-//   }
-//   else if (signs === "-"){
-//     return num1 - num2;
-//   }
-//   else if (signs === "+"){
-//     return num1 + num2;
-//   }
-// }
+function giveResults(){
+  console.log("Give me a moment!");
+}
